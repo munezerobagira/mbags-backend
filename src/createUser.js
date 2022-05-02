858e53abaae16ccfdfcea3d3b22880d98c9da0a7
Sostene Munezero Bagira <munezerobagira@gmail.com>
2022-05-02 16:57:55 +0200

    Prevent the breakdown

--- src/createUser.js
+++ src/createUser.js
@@ -4,13 +4,17 @@ import { mongoUrl } from "./config";
 
 const main = async () => {
   await mongoose.connect(mongoUrl);
-  const user = await createUser({
-    name: "Sostene MUnezero Bagira",
-    email: "bagira.sostenee@gmail.com",
-    username: "password123",
-    password: "password123",
-    role: "admin",
-  });
-  if (user) console.log("User created successfully");
+  try {
+    const user = await createUser({
+      name: "Sostene MUnezero Bagira",
+      email: "bagira.sostenee@gmail.com",
+      username: "password123",
+      password: "password123",
+      role: "admin",
+    });
+    if (user) console.log("User created successfully");
+  } catch (err) {
+    console.log("Admin already exists");
+  }
 };
 main();
